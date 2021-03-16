b66e4d836412b2fdd7165871b1ee7acfbd2ef365
Asuwathaman R C <53341232+AsuwathamanRC@users.noreply.github.com>
2021-03-16 20:50:13 +0530

    Update bandNameGenerator.py
    Added comment

--- bandNameGenerator.py
+++ bandNameGenerator.py
@@ -1,3 +1,6 @@
+#Band Name Generator
+#Generate band name from the user's city & pet's name
+
 print("Welcome to Band Name Generator")
 city = input("which city you grew up in?\n")
 petName = input("What is your pet's name?\n")
