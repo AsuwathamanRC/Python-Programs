bf09fc9f0b65ede8a899d10401344e251480fdf2
Asuwathaman R C <53341232+AsuwathamanRC@users.noreply.github.com>
2021-12-25 20:03:18 +0530

    Added new program - Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -27,3 +27,41 @@ for thing in programming_dictionary:
 #wipe an existing dictionary
 programming_dictionary = {}
 print(programming_dictionary)
+
+
+#######################################
+
+#Nesting 
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+#Nesting a List in a Dictionary
+
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+#Nesting Dictionary in a Dictionary
+
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+#Nesting Dictionaries in Lists
+
+travel_log = [
+{
+  "country": "France", 
+  "cities_visited": ["Paris", "Lille", "Dijon"], 
+  "total_visits": 12,
+},
+{
+  "country": "Germany",
+  "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+  "total_visits": 5,
+},
+]
