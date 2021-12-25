62f6d55fb9980a5bf3a854a8a24452ff05f40c45
Asuwathaman R C <53341232+AsuwathamanRC@users.noreply.github.com>
2021-12-25 21:56:40 +0530

    Added new program - Calculator

---
+++ Calculator.py
@@ -0,0 +1,42 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+def calculate(first):
+    operation = input("Pick an operation ( +, -, *, /): ")
+    second = float(input("Enter the second number: "))
+
+    if operation == "+":
+        result = first+second
+    elif operation == "-":
+        result = first-second
+    elif operation == "*":
+        result = first*second
+    elif operation == "/":
+        result = first/second
+    else:
+        return "Invalid operator"
+
+    print(f"{first} {operation} {second} = {result}")
+    bFlag = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+    if bFlag=="y":
+        calculate(result)
+    else:
+        return
+
+first = float(input("Enter the first number: "))
+calculate(first)
