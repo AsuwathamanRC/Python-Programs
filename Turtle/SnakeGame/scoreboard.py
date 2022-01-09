32f4c20f4048f401903a80bf4e8ff785810e5efe
Asuwathaman R C <53341232+AsuwathamanRC@users.noreply.github.com>
2022-01-09 16:06:56 +0530

    Save highscore in local file - Classic snake game

--- Turtle/SnakeGame/scoreboard.py
+++ Turtle/SnakeGame/scoreboard.py
@@ -10,7 +10,8 @@ class Scoreboard(Turtle):
         self.goto(0,280)
         self.hideturtle()
         self.score = 0
-        self.highScore = 0
+        with open("Turtle/SnakeGame/data.txt") as f:
+            self.highScore = int(f.read())
         self.updateScoreboard()
 
     def updateScoreboard(self):
@@ -25,6 +26,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score>self.highScore:
             self.highScore = self.score
+            with open("Turtle/SnakeGame/data.txt","w") as f:
+                f.write(str(self.score))
+
         self.score = 0
         self.updateScoreboard()
 
